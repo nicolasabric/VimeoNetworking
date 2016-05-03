4b33901c8518477e93e0fa0f0c79eaff8fc6ade3
Jason Hawkins <hellojason@me.com>
2016-05-03 14:51:06 -0400

    Adds VimeoNetworking-Bridging-Header.

---
+++ VimeoNetworking/VimeoNetworking-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  VimeoNetworking-Bridging-Header.h
+//  VimeoNetworking
+//
+//  Created by Hawkins, Jason on 5/3/16.
+//  Copyright © 2016 Vimeo. All rights reserved.
+//
+
+#ifndef VimeoNetworking_Bridging_Header_h
+#define VimeoNetworking_Bridging_Header_h
+
+#import "VIMVideo.h"
+
+#endif /* VimeoNetworking_Bridging_Header_h */
